078fefbbf43b2b64f00dff91c483af34976e8164
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-17 17:00:02 +0100

    add second section in AboutMePage.js

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import AMPimage from '../images/AMPimage.png';
-
 import '../styles/pages-styles/AboutMePage.css';
+
+import AMPimage from '../images/AMPimage.png';
+import AMPimage2 from '../images/AMPimage2.jpg';
 
 const AboutMePage = () => {
     return ( 
         <div className="AboutMePage">
             <h1>O mnie...</h1>
-            <header>
+            <section className="first">
                 <img src={AMPimage} alt='AMPimage'/>
                 <article>
                     <h1>Julia Gamelska</h1>
@@ -21,18 +22,25 @@ const AboutMePage = () => {
                     <br/>
                     <h2>Kształcę się, szkole dla <u>CIEBIE!</u></h2>
                 </article>
-            </header>
+            </section>
 
-            {/* 
-            <h3>Jestem certyfikowanym trenerem personalnym oraz instruktorem siłowni. Odbyłam kursy treningu
-            TRX, treningu POWER BAND i MINI BAND, kształtowania kobiecych pośladów, treningu KETTLEBELL,
-            rolowania, stretchingu statycznego i dynamicznego oraz szkolenie dotyczące zdrowego żywienia,
-            dietetyki i dietetyki sportowej.</h3>
-            <h3>Dlaczego jestem skuteczna?</h3>
-            <h2>Bo skupiam się na Tobie!</h2> 
-            <h3>W pracę wkładam całe serce- jest to moja
-            największa pasja i nigdy nie czuję się źle w pracy!</h3> 
-            <h2>Warto mi zaufać!</h2> */}
+            <h1>Doświadczenie</h1>
+            <section className="twice">
+                <article>
+                    <span>
+                        Jestem certyfikowanym trenerem personalnym oraz instruktorem siłowni. Odbyłam kursy treningu
+                        TRX, treningu POWER BAND i MINI BAND, kształtowania kobiecych pośladów, treningu KETTLEBELL,
+                        rolowania, stretchingu statycznego i dynamicznego oraz szkolenie dotyczące zdrowego żywienia,
+                        dietetyki i dietetyki sportowej.
+                        Dlaczego jestem skuteczna?
+                        Bo skupiam się na Tobie!
+                        W pracę wkładam całe serce- jest to moja
+                        największa pasja i nigdy nie czuję się źle w pracy!
+                    </span>
+                    <h2>Warto mi zaufać!</h2>
+                </article>                
+                <img src={AMPimage2} alt="AMPimage2"/>
+            </section>
         </div>
      );
 }
