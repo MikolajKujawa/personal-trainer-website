fd87da81fab18c53c667ac73da134f15efb11505
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-12-01 20:47:39 +0100

    add link for movie on YouTube change intro add global font color

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -10,7 +10,7 @@ const Media = () => {
         <div className="mediabox">
             <a href="https://www.facebook.com/gold.fit.factory.julia/" target="blank"><img src={facebook} alt="facebook"/></a>
             <a href="https://www.instagram.com/gold_fit_factory_julia/" target="blank"><img src={instagram} alt="instagram"/></a>
-            <a href="https://youtube.com" target="blank"><img src={youtube} alt="youtube"/></a>
+            <a href="https://youtu.be/0BVDjj8ixSU" target="blank"><img src={youtube} alt="youtube"/></a>
         </div>
      );
 }
--- src/layouts/Intro.jsx
+++ src/layouts/Intro.jsx
@@ -4,7 +4,7 @@ import '../styles/layouts-styles/Intro.sass';
 const Intro = () => {
     return (
         <div className="video-bg">
-            <iframe title="promotion video" width="560" height="315" src="https://www.youtube.com/embed/0BVDjj8ixSU?modestbranding=1&autoplay=1&controls=0&loop=1&playlist=0BVDjj8ixSU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="promotion video" width="560" height="315" src="https://www.youtube.com/embed/0BVDjj8ixSU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
